65a914885f8b350ef935fd337851a98d0867d0bb
Paulo-Sergio <paulofranca.contato@gmail.com>
2018-06-06 16:02:11 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/br/com/paulofranca/cursomc/CursomcApplication.java
+++ src/main/java/br/com/paulofranca/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package br.com.paulofranca.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import br.com.paulofranca.cursomc.model.Cidade;
 import br.com.paulofranca.cursomc.model.Cliente;
 import br.com.paulofranca.cursomc.model.Endereco;
 import br.com.paulofranca.cursomc.model.Estado;
+import br.com.paulofranca.cursomc.model.Pagamento;
+import br.com.paulofranca.cursomc.model.PagamentoComBoleto;
+import br.com.paulofranca.cursomc.model.PagamentoComCartao;
+import br.com.paulofranca.cursomc.model.Pedido;
 import br.com.paulofranca.cursomc.model.Produto;
+import br.com.paulofranca.cursomc.model.enums.EstadoPagamento;
 import br.com.paulofranca.cursomc.model.enums.TipoCliente;
 import br.com.paulofranca.cursomc.repositories.CategoriaRepository;
 import br.com.paulofranca.cursomc.repositories.CidadeRepository;
 import br.com.paulofranca.cursomc.repositories.ClienteRepository;
 import br.com.paulofranca.cursomc.repositories.EnderecoRepository;
 import br.com.paulofranca.cursomc.repositories.EstadoRepository;
+import br.com.paulofranca.cursomc.repositories.PagamentoRepository;
+import br.com.paulofranca.cursomc.repositories.PedidoRepository;
 import br.com.paulofranca.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -39,12 +47,18 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
 
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -63,7 +77,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+
 		System.out.println("------- Adicionando Categorias e Produtos --------------");
 		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		this.produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
@@ -83,19 +97,36 @@ public class CursomcApplication implements CommandLineRunner {
 		this.estadoRepository.saveAll(Arrays.asList(est1, est2));
 		this.cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		System.out.println("----------- FIM --------------");
-		
+
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
-		
+
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
-		
+
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
-		
+
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
-		
+
 		System.out.println("------- Adicionando Clientes e Endereços --------------");
 		this.clienteRepository.saveAll(Arrays.asList(cli1));
 		this.enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		System.out.println("----------- FIM --------------");
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		System.out.println("------- Adicionando Pedidos e Pagamentos --------------");
+		this.pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		this.pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		System.out.println("----------- FIM --------------");
 	}
 }
--- src/main/java/br/com/paulofranca/cursomc/model/Cliente.java
+++ src/main/java/br/com/paulofranca/cursomc/model/Cliente.java
@@ -35,8 +35,12 @@ public class Cliente implements Serializable {
 	private List<Endereco> enderecos = new ArrayList<>();
 
 	@ElementCollection
-	@CollectionTable(name = "TELEFONE") // vai ser gerada a tebela telefone com cliente_id
-	private Set<String> telefones = new HashSet<>();
+	@CollectionTable(name = "TELEFONE")
+	private Set<String> telefones = new HashSet<>();// vai ser gerada a tebela
+													// telefone com cliente_id
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 
 	public Cliente() {
 	}
@@ -104,6 +108,14 @@ public class Cliente implements Serializable {
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	@Override
