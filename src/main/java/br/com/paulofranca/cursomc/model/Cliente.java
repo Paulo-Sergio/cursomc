446c3b1945887e50a09560dcfb7594e3cb968f67
Paulo-Sergio <paulofranca.contato@gmail.com>
2018-06-07 10:57:39 -0300

    Endpoint /pedidos/{id} desponibilizado

--- src/main/java/br/com/paulofranca/cursomc/model/Cliente.java
+++ src/main/java/br/com/paulofranca/cursomc/model/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import br.com.paulofranca.cursomc.model.enums.TipoCliente;
@@ -39,6 +40,7 @@ public class Cliente implements Serializable {
 	private Set<String> telefones = new HashSet<>();// vai ser gerada a tebela
 													// telefone com cliente_id
 
+	@JsonBackReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 
